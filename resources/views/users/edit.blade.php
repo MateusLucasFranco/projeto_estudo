b1e7a0f5c00319d0c1617d7abcc07153f6f1ef17
MateusLucasFranco <mateusllucasfranco@gmail.com>
2024-04-29 16:15:06 -0300

    [UPD] padronizando componente de formulario

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -8,33 +8,10 @@
                     <h1 class="card-title">Editar o Usuário {{$user->name}}</h1>
                 </div>
                 <div class="card-body">
-                    @if($errors->any())
-                    <ul class="errors">
-                        @foreach($errors->all() as $error)
-                        <li class="error">{{$error}}</li>
-                        @endforeach
-                    </ul>
-                    @endif
+                    @include('includes.validations-form')
                     <form method="POST" action="{{route('users.update', $user->id)}}">
                         @method('PUT')
-                        @csrf
-                        <div class="input-group mb-3">
-                            <span class="input-group-text" id="basic-addon1"><i class="fas fa-user-plus"></i></span>
-                            <input type="text" name="name" class="form-control" placeholder="Nome" aria-label="Nome" aria-describedby="basic-addon1" value="{{ $user->name}}">
-                        </div>
-                
-                
-                        <div class="input-group mb-3">
-                            <span class="input-group-text" id="basic-addon1"><i class="fas fa-envelope"></i></span>
-                            <input type="email" name="email" class="form-control" placeholder="Email" aria-label="Email" aria-describedby="basic-addon1" value="{{ $user->email}}">
-                        </div>
-                
-                        <div class="input-group mb-3">
-                            <span class="input-group-text" id="basic-addon1"><i class="fas fa-key"></i></span>
-                            <input type="password" name="password" class="form-control" placeholder="Nova senha" aria-label="Senha" aria-describedby="basic-addon1">
-                        </div>
-
-                        <button class="btn btn-outline-primary"><i class="fas fa-save"></i> Enviar</button>
+                        @include('users._partials.form')
                     </form>
                 </div>
             </div>
