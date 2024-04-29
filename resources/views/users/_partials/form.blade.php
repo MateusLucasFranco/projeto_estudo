b9fcdbb9fdc03b1078040705b36c046fd6edbf93
MateusLucasFranco <mateusllucasfranco@gmail.com>
2024-04-29 16:15:38 -0300

    [NEW] componente para formulario

---
+++ resources/views/users/_partials/form.blade.php
@@ -0,0 +1,18 @@
+@csrf
+<div class="input-group mb-3">
+    <span class="input-group-text" id="basic-addon1"><i class="fas fa-user-plus"></i></span>
+    <input type="text" name="name" class="form-control" placeholder="Nome" aria-label="Nome" aria-describedby="basic-addon1" value="{{ $user->name ?? old('name')}}">
+</div>
+
+
+<div class="input-group mb-3">
+    <span class="input-group-text" id="basic-addon1"><i class="fas fa-envelope"></i></span>
+    <input type="email" name="email" class="form-control" placeholder="Email" aria-label="Email" aria-describedby="basic-addon1" value="{{ $user->email ?? old('email')}}">
+</div>
+
+<div class="input-group mb-3">
+    <span class="input-group-text" id="basic-addon1"><i class="fas fa-key"></i></span>
+    <input type="password" name="password" class="form-control" placeholder="Nova senha" aria-label="Senha" aria-describedby="basic-addon1">
+</div>
+
+<button class="btn btn-outline-primary"><i class="fas fa-save"></i> Enviar</button>
