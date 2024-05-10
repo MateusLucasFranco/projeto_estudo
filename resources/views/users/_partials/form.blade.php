eefecd446a7c410472995f73bce82f9284b2cddc
MateusLucasFranco <mateusllucasfranco@gmail.com>
2024-05-10 16:47:18 -0300

    [NEW] campo image

--- resources/views/users/_partials/form.blade.php
+++ resources/views/users/_partials/form.blade.php
@@ -15,4 +15,9 @@
     <input type="password" name="password" class="form-control" placeholder="Nova senha" aria-label="Senha" aria-describedby="basic-addon1">
 </div>
 
+<div class="input-group mb-3">
+    <span class="input-group-text" id="basic-addon1"><i class="fas fa-image"></i></span>
+    <input type="file" name="image" class="form-control">
+</div>
+
 <button class="btn btn-outline-primary"><i class="fas fa-save"></i> Enviar</button>
