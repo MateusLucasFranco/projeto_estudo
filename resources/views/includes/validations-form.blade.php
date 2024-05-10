7f48672a69fcaba103b5746c4b2a5bb145aa597a
MateusLucasFranco <mateusllucasfranco@gmail.com>
2024-05-10 16:46:50 -0300

    [NEW] sweetalert

--- resources/views/includes/validations-form.blade.php
+++ resources/views/includes/validations-form.blade.php
@@ -1,7 +1,9 @@
+@include('sweetalert::alert')
+
 @if($errors->any())
     <ul class="errors">
         @foreach($errors->all() as $error)
-            <li class="error">{{ $error}}</li>
+            <li class="error">{{ $error }}</li>
         @endforeach
     </ul>
 @endif
